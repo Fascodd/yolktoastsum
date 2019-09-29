487e83507bd5c95f750076beea78b70246018e5c
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-09-28 19:24:14 -0700

    testing deployment
    Testing deployment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          TRAVIS IS COOL
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
