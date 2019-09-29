280a9691429e074bea0fb049d5554c1add241335
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-09-29 10:57:13 -0700

    Simple Text for site
    Changed out logo and links for React to simply read, "EAGLE EATS BUTTS"

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>item1</li>
+                    <li>item2</li>
+                    <li>item3</li>
+                </ul>
+            </div>
+        )
+    }
+}
