e1c91e16b0e8c870fc7a42780a3942e4d554a7a3
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-09-29 13:33:51 -0700

    Inital Setup of containers
    Setup of container divs and navbar
    added color to backgrounds
    
    top container
    mid container
    bottom container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          EAGLE EATS BUTTS
-        </p>
-      
-      </header>
+      <div className="top-container">
+        <div className="navbar">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="central-container">
+        <div className="l-nav-bar"></div>
+        <div className="main-show"></div>
+      </div>
+
+      <div className="bottom-container">
+        <div className="bottom-bar"></div>
+      </div>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react"
-class Navbar extends React.Component{
-    render(){
-        return(
+import "../Styles/Navbar.css"
+class Navbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            navHeader: [{ name: "item1", ID: 1 }, { name: "item2", ID: 2 }, { name: "item1", ID: 3 }]
+        }
+    }
+    render() {
+        return (
             <div>
-                <ul>
-                    <li>item1</li>
-                    <li>item2</li>
-                    <li>item3</li>
+                <ul className="main-nav">
+                    {this.state.navHeader.map(item => {
+                        return <li key={item.ID} className="navHeader-li"> {item.name} </li>
+                    })}
                 </ul>
             </div>
         )
     }
 }
+export default Navbar 
